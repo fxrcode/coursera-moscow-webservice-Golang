30c80c680d84c40827e51d0be15dba19949a2d80
fxrcode <fxrcode@gmail.com>
2020-10-21 23:53:46 -0700

    here comes the select to multiplexing rather loop

--- async/select_1.go
+++ async/select_1.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	ch1 := make(chan int, 1)
+	ch1 := make(chan int)
+	// ch1 := make(chan int, 1)
 	ch2 := make(chan int)
 	// ch1 <- 1
 	select {
@@ -13,7 +14,7 @@ func main() {
 		fmt.Println("ch1 val", val)
 	case ch2 <- 1:
 		fmt.Println("put val to ch2")
-		// default:
-		// 	fmt.Println("default case")
+	default:
+		fmt.Println("default case")
 	}
 }
